642d3413b8a665876e307b52c73df4bdae03458d
Adenilson Neto <132957192+adenilsonneto@users.noreply.github.com>
2024-10-07 18:36:31 -0300

    Add files via upload

---
+++ CemiterioDeEspiritos.java
@@ -0,0 +1,17 @@
+package cartas;
+
+public class CemiterioDeEspiritos extends Encantamentos{
+
+    public CemiterioDeEspiritos(){
+        super();
+        setNome("Cemitério de Espiritos");
+        setMana(5);
+
+    }
+    
+    public void invocarEspirito(Criatura espirito){
+        for(int i = 0; i < length(carta.Cemitério); i++ ){
+            
+        }
+    }
+}
