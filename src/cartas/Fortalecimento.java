2127fbbf78e01b10da57ec16f5bfe82442898d42
Adenilson Neto <132957192+adenilsonneto@users.noreply.github.com>
2024-11-02 20:39:34 -0300

    Update Fortalecimento.java

--- src/cartas/Fortalecimento.java
+++ src/cartas/Fortalecimento.java
@@ -1,11 +1,13 @@
 package cartas;
-
+        
 public class Fortalecimento extends Encantamentos{
-    public Fortalecimento(){
+    public Fortalecimento(){       
         super();
         setMana(3);
-        setNome(" Fortalecimento");
-        setEfeitoContinuo(1);
+        setNome("Fortalecimento");
+        danoEfeitoContinuo(1);
+        setValidacao("Buff");
+
     }
 
 }
