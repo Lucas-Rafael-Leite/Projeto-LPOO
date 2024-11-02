dd0fe58ad4a4445e63c3b4274823fb3c3e62ef3a
Adenilson Neto <132957192+adenilsonneto@users.noreply.github.com>
2024-11-02 19:28:10 -0300

    Update VelocidadeDeAtaque.java

--- src/cartas/VelocidadeDeAtaque.java
+++ src/cartas/VelocidadeDeAtaque.java
@@ -1,9 +1,14 @@
 package cartas;
 
 public class VelocidadeDeAtaque extends Encantamentos {
-	 public VelocidadeDeAtaque(){
-	        super();
-	        setNome(" Velocidade de Ataque");
-	        setMana(3);
-	    }
+    public VelocidadeDeAtaque(){
+        super();
+        setNome("Velocidade de Ataque");
+        setMana(3);
+
+    }
+
+    public void velocidadeAtaque(Criatura destino){
+        destino.ataque(getPoder()*2); ;
+    }
 }
