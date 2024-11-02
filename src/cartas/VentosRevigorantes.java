8e6ab51f532d2cc4d3bf2fa74eba7c85e15cb62b
Adenilson Neto <132957192+adenilsonneto@users.noreply.github.com>
2024-11-02 20:39:00 -0300

    Update VentosRevigorantes.java

--- src/cartas/VentosRevigorantes.java
+++ src/cartas/VentosRevigorantes.java
@@ -1,11 +1,13 @@
 package cartas;
 
-public class VentosRevigorantes extends Encantamentos {
-	 public VentosRevigorantes(){
-	        super();
-	        setNome(" Ventos Revigorantes");
-	        setMana(4);
-	        danoEfeitoContinuo(-1);    
-	    }
+public class VentosRevigorantes extends Encantamentos{
 
+    public VentosRevigorantes(){
+        super();
+        setNome("Ventos Revigorantes");
+        setMana(4);
+        danoEfeitoContinuo(-1);   
+        setValidacao("Buff"); 
+    }
+    
 }
