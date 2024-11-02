df048b425a62ccc23b6a8c6063abe8ea1de0a325
Adenilson Neto <132957192+adenilsonneto@users.noreply.github.com>
2024-11-02 01:15:03 -0300

    Update BolaDeFogo.java

--- BolaDeFogo.java
+++ BolaDeFogo.java
@@ -2,6 +2,8 @@ package cartas;
 
 public class BolaDeFogo extends Feiticos{
 
+    private int turnoJogada;
+
     public BolaDeFogo(){
         super();
         setNome("Bola de Fogo");
@@ -9,10 +11,24 @@ public class BolaDeFogo extends Feiticos{
         danoEfeito(1);
     }
     
-    public void queimaduras(Criatura destino){
+    public void jogarFeitico(Criatura destino, int trunoAtual){
+
+        this.turnoJogada = trunoAtual;
+        queimaduras(destino, trunoAtual);
+
+    }
+   
+    public void queimaduras(Criatura destino, int turnoAtual){
         
-       
+        if(turnoAtual == turnoJogada + 1){
+
+            destino.danoEfeito(1);
+        
+        }
+        
+        
 
     }
 
 }
+
