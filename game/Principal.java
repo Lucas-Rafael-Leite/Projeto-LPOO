bf4251b90fa9880d9b2accf4fef247a934870e02
Lucas-Rafael-Leite <lucas.rleite@upe.br>
2024-11-07 21:40:06 -0300

    Add files via upload

---
+++ game/Principal.java
@@ -0,0 +1,41 @@
+package game;
+
+import java.util.Scanner;
+
+import menu.FimDaBatalha;
+import menu.MontadorDeck;
+import menu.NomearJogadores;
+import arenas.ArenaCombate;
+import excecoes.CreatureCannotAttackException;
+import excecoes.EmptyDeckException;
+import excecoes.ManaInsuficienteException;
+
+public class Principal {
+	public static void main(String[] args) throws ManaInsuficienteException, CreatureCannotAttackException, EmptyDeckException {
+		NomearJogadores nomeio = new NomearJogadores();
+		MontadorDeck deck = new MontadorDeck();
+		ArenaCombate arena = new ArenaCombate();
+		FimDaBatalha batalhaFim = new FimDaBatalha();
+		Scanner ler = new Scanner(System.in);
+		nomeio.nomearJogadores(ler);
+		nomeio.transferirJogadores(deck);
+		deck.escolherCarta(ler);
+		deck.transferirJogadores(arena);
+		while(batalhaFim.getVerdade() == true) {
+		arena.vezJogador(ler);
+		arena.jogadoresResetDeck();
+		arena.transferirJogadores(batalhaFim);
+		arena.removerJogadores();
+		batalhaFim.fimDeJogo(ler);
+		batalhaFim.transferirJogadores(arena);
+		batalhaFim.removerJogadores();
+		arena.resetTurno();
+		}
+		ler.close();
+	}
+}
+		
+		
+
+
+
