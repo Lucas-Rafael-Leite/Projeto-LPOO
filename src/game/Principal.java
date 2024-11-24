3a8d5c95b185a0853b6bbbdda3661c245c9ffb93
Lucas-Rafael-Leite <lucas.rleite@upe.br>
2024-11-24 14:32:35 -0300

    Update Principal.java

--- src/game/Principal.java
+++ src/game/Principal.java
@@ -1,41 +1,25 @@
 package game;
 
-import java.util.Scanner;
+import javax.swing.SwingUtilities;
 
-import menu.FimDaBatalha;
-import menu.MontadorDeck;
-import menu.NomearJogadores;
-import arenas.ArenaCombate;
+
+import menu.TelaMenu;
 import excecoes.CreatureCannotAttackException;
 import excecoes.EmptyDeckException;
 import excecoes.ManaInsuficienteException;
 
 public class Principal {
 	public static void main(String[] args) throws ManaInsuficienteException, CreatureCannotAttackException, EmptyDeckException {
-		NomearJogadores nomeio = new NomearJogadores();
-		MontadorDeck deck = new MontadorDeck();
-		ArenaCombate arena = new ArenaCombate();
-		FimDaBatalha batalhaFim = new FimDaBatalha();
-		Scanner ler = new Scanner(System.in);
-		nomeio.nomearJogadores(ler);
-		nomeio.transferirJogadores(deck);
-		deck.escolherCarta(ler);
-		deck.transferirJogadores(arena);
-		while(batalhaFim.getVerdade() == true) {
-		arena.vezJogador(ler);
-		arena.jogadoresResetDeck();
-		arena.transferirJogadores(batalhaFim);
-		arena.removerJogadores();
-		batalhaFim.fimDeJogo(ler);
-		batalhaFim.transferirJogadores(arena);
-		batalhaFim.removerJogadores();
-		arena.resetTurno();
-		}
-		ler.close();
+		   SwingUtilities.invokeLater(() -> {
+	            TelaMenu telaMenu = new TelaMenu();
+	            telaMenu.setVisible(true);
+	        });
+	    
 	}
 }
 		
 		
+
 
 
 
