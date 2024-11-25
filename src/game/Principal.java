3d0f7fd82aa8f70dd3cfd52e2648a87e73f7a4ac
Adenilson Neto <132957192+adenilsonneto@users.noreply.github.com>
2024-11-25 17:06:01 -0300

    Update Principal.java

--- src/game/Principal.java
+++ src/game/Principal.java
@@ -7,12 +7,18 @@ import menu.TelaMenu;
 import excecoes.CreatureCannotAttackException;
 import excecoes.EmptyDeckException;
 import excecoes.ManaInsuficienteException;
+import menu.MontadorDeckTela;
+import menu.NomearJogadoresTela;
 
 public class Principal {
 	public static void main(String[] args) throws ManaInsuficienteException, CreatureCannotAttackException, EmptyDeckException {
 		   SwingUtilities.invokeLater(() -> {
 	            TelaMenu telaMenu = new TelaMenu();
 	            telaMenu.setVisible(true);
+                    NomearJogadoresTela nomearJogadores = new NomearJogadoresTela();
+                    nomearJogadores.setVisible(true);
+                    MontadorDeckTela montadorDeck = new MontadorDeckTela();
+                    montadorDeck.setVisible(true);
 	        });
 	    
 	}
